78b91f59bfc30f7ad616d6a7ca1dc3759d719b22
Levi <44029095+Toour@users.noreply.github.com>
2021-10-17 13:58:04 +0300

    Add files via upload
    Python Değişken Tanımlama

---
+++ DegiskenTanimlama.py
@@ -0,0 +1,16 @@
+_musteriAdi = "Turan"
+_musteriSoyadi = "Kılıç"
+_musteriAdSoyad = _musteriAdi + " " + _musteriSoyadi
+_musteriCinsiyeti = "E"
+_musteriTC = "35345345340"
+_musteriDogumYili = 1999
+_musteriAdres = "Ankara"
+_musteriYasi = 2021 - _musteriDogumYili
+
+_order1 = 110
+_order2 = 1100.5
+_order3 = 356.95
+
+print(_order1 + _order2 + _order3)
+
+
