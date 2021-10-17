9eaa2e6fa0380ff196e5d38cb867a4ee963e6b49
Levi <44029095+Toour@users.noreply.github.com>
2021-10-17 14:22:31 +0300

    Add files via upload

---
+++ UserInput.py
@@ -0,0 +1,9 @@
+r = int(input("Dairenin Yarıçapını Giriniz: "))
+pi = 3.14
+daireAlan = pi * (r**2)
+daireCevre = 2 * pi * r
+
+
+print("## Girilen Yarıçapa Sahip ##")
+print("Dairenin Alanı: " + str(daireAlan))
+print("Dairenin Çevresi: " + str(daireCevre))
