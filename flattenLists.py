9d66a5f58f5b7a4f1f0eebf6daf3c70c893fb9a0
Levi <44029095+Toour@users.noreply.github.com>
2021-11-02 20:22:07 +0300

    Create flattenLists.py
    Creating flatten list from complex lists

---
+++ flattenLists.py
@@ -0,0 +1,22 @@
+from typing import List
+
+l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flat_list2 = []
+
+def lookInside(l):                  # Takes the list type element
+    for x in l:                     # For each sub-element of it
+        if type(x) is not list:     # If the sub-element is not list, 
+            flat_list2.append(x)    # Then add it to flat_list
+        else:                       
+            lookInside(x)           # Else, look inside of it again
+
+# WE ARE STARTING HERE !!!
+def makeFlat(l):                    # Getting the list 
+    for e in l:                     # Checking the elements of the list
+        if type(e) is list:         # If element's type is list then
+            lookInside(e)           # send that element to lookInside function 
+        else:
+            flat_list2.append(e)    # Else, (if it is not list) append it to our new flat_list
+
+makeFlat(l)                         # Function call, the complex list has been given to function
+print(flat_list2)                   # Printing the flatten list
